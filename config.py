25c404862adc307ee1e8e54c5d2951cbe5de5f66
F44101313 <144661785+F44101313@users.noreply.github.com>
2025-08-25 03:14:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame #config.py
 
 # --- 視窗設定 ---
 WIDTH, HEIGHT = 600, 800
@@ -8,6 +8,7 @@ WHITE = (255, 255, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
 
 # --- 遊戲參數 ---
 PLAYER_SPEED = 5
@@ -18,7 +19,9 @@ CASTLE_HP = 1000
 
 # 字型
 pygame.font.init()
-font = pygame.font.SysFont("Arial", 24)
+font = pygame.font.Font("Font/BoutiqueBitmap9x9_Bold_1.9.ttf", 18)
+# 新增大字體給標題
+title_font = pygame.font.Font("Font/BoutiqueBitmap9x9_Bold_1.9.ttf", 48)  # 48 可依需求調整大小
 
 # --- 金錢與升級 ---
 ENEMY_REWARD = {10: 10, 20: 20, 30: 30}  # HP 對應獎金
